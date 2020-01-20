b1f94a665eea7524556cff8087adf73630fa3165
Andrey Bolshakov <abolshakovy@gmail.com>
2020-01-20 22:50:14 +0300

    Add CRUD

--- blog/tests.py
+++ blog/tests.py
@@ -1,8 +1,8 @@
-from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
 from django.urls import reverse
 
-from blog.models import Post
+from .models import Post
 
 
 class BlogTests(TestCase):
@@ -15,15 +15,17 @@ class BlogTests(TestCase):
         )
 
         self.post = Post.objects.create(
-            title = 'A good title',
-            body = 'Nice body content',
-            author=self.user
+            title='A good title',
+            body='Nice body content',
+            author=self.user,
         )
 
-    def test_string_representation(self):
-        post = Post(title='A simple title')
-        self.assertEqual(str(post), post.title)
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(),'/post/1/')
 
+    def test_string_representation(self):
+        post = Post(title='A sample title')
+        self.assertEqual(str(post), post.title)
 
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
@@ -36,10 +38,32 @@ class BlogTests(TestCase):
         self.assertContains(response, 'Nice body content')
         self.assertTemplateUsed(response, 'home.html')
 
-    def posts_detail_view(self):
+    def test_post_detail_view(self):
         response = self.client.get('/post/1/')
-        no_response = self.client.get('/post/10000/')
+        no_response = self.client.get('/post/100000/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
